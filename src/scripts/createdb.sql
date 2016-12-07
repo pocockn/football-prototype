9e16e55d985958160a393d95a48be1e65911865a
Nick Pocock <pocockn@hotmail.co.uk>
2016-12-07 15:56:32 +0000

    @pocockn edited gitignore

---
+++ src/scripts/createdb.sql
@@ -0,0 +1,5 @@
+create user pocockn;
+alter user pocockn superuser;
+drop database footballPrototype;
+create database footballPrototype;
+grant all privileges on database footballPrototype to pocockn;
