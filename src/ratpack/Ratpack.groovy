84cee777ba6f7594adafed24a8331a8a15b80bf9
Nick Pocock <nick.pocock@informa.com>
2016-12-08 09:02:55 +0000

    @pocockn on initilisation of application log out to terminal

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -3,6 +3,10 @@ import models.Player
 import models.Team
 import ratpack.groovy.template.MarkupTemplateModule
 import ratpack.handlebars.HandlebarsModule
+import ratpack.service.Service
+import ratpack.service.StartEvent
+
+import java.util.logging.Logger
 
 import static ratpack.groovy.Groovy.groovyMarkupTemplate
 import static ratpack.groovy.Groovy.ratpack
@@ -14,6 +18,12 @@ ratpack {
         bind HighChartHandler
         bind Player
         bind Team
+        bindInstance new Service() {
+            void onStart(StartEvent e) throws Exception {
+                Logger logger = Logger.getLogger("")
+                logger.info("Initialising Football Prototype")
+            }
+        }
     }
 
     handlers {
