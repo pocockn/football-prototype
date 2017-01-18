f83473e02316b3014e75fcd74d120774d80f23f5
Nick Pocock <pocockn@hotmail.co.uk>
2017-01-18 11:57:14 +0000

    changed create db script to add test database

--- src/scripts/createdb.sql
+++ src/scripts/createdb.sql
@@ -3,3 +3,8 @@ alter user pocockn superuser;
 drop database footballPrototype;
 create database footballPrototype;
 grant all privileges on database footballPrototype to pocockn;
+
+drop database test;
+create database test;
+grant all privileges on database test to pocockn;
+grant all privileges on database test to pocockn;
