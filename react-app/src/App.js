8cbd3d887c5b426f483bca12281dd63f84a29cfa
Nick Pocock <pocockn@hotmail.co.uk>
2017-01-23 22:28:15 +0000

    first working prototype of fetching data from the ratpack api

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            teams : []
+        };
+    }
+
+    componentDidMount() {
+        axios.get("/api/teams")
+            .then(res => {
+                const teams = res.data;
+                this.setState({teams});
+                console.log(teams);
+            });
+    }
 
   render() {
     return (
@@ -11,6 +28,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+        <h2>Teams</h2>
+        <ul>
+            {this.state.teams.map(singleTeam =>
+                <li key={singleTeam.id}>{singleTeam.team.name}</li>
+            )}
+            <button>delete</button>
+        </ul>
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
--- site/src/main/groovy/api/TeamGetHandlerApi.groovy
+++ site/src/main/groovy/api/TeamGetHandlerApi.groovy
@@ -1,14 +1,16 @@
 package api
 
+import groovy.util.logging.Slf4j
 import ratpack.handling.Context
 import ratpack.handling.InjectionHandler
 import service.persistance_service.TeamStoreService
 
 import static ratpack.jackson.Jackson.json
-
+@Slf4j
 class TeamGetHandlerApi extends InjectionHandler {
     void handle(Context ctx, TeamStoreService teamStoreService) {
         teamStoreService.fetchAll().then {
+            log.info("retrieved data via react")
             ctx.render json(it)
         }
     }
