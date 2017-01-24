0a10272daefe968ee9b781dbcb8c9afdeaffb11f
Nick Pocock <nick.pocock@informa.com>
2017-01-24 15:16:36 +0000

    start to use react router for our one page admin app

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Router, Route, hashHistory } from 'react-router'
+import Sidebar from "./components/Sidebar.js";
+import { browserHistory } from 'react-router'
+import "./index.css";
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <Router history={browserHistory}>
+        <Route path="/" component={App}/>
+    </Router>,
+    document.body
 );
