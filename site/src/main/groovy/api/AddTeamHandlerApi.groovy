724eedb0797f40dd66f54fb61c5dfc37afcbc172
pocockn <pocockn@hotmail.co.uk>
2017-09-07 21:34:05 +0100

    generic parse and validate method to be used within API handlers

--- site/src/main/groovy/api/AddTeamHandlerApi.groovy
+++ site/src/main/groovy/api/AddTeamHandlerApi.groovy
@@ -17,7 +17,7 @@ class AddTeamHandlerApi extends InjectionHandler {
     void handle(Context context, TeamStoreService teamStoreService) {
         context.byMethod {
             it.post {
-                context.parse(fromJson(Team))
+                parseAndValidate(context, Team)
                         .then { team ->
                     TeamContainer teamContainer = new TeamContainer(id: team.id, team: team)
                     teamStoreService
@@ -31,11 +31,12 @@ class AddTeamHandlerApi extends InjectionHandler {
     }
 //http://mrhaki.blogspot.co.uk/2015/11/ratpack-validating-forms.html
     // http://kyleboon.org/blog/2016/06/30/validating-an-http-post-with-ratpack/
+
     private <T> Promise<T> parseAndValidate(Context context, Class<T> type) {
         context.parse(fromJson(type)).route({ T obj ->
             context.get(Validator).validate(obj).size() > 0
         }, {
-
+            context.clientError(422)
         })
     }
 }
